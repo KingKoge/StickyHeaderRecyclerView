6555e2495d4218c1498b8552473f70b24ed04fcc
Suttichai Pawapaocapinun <suttichai@livingmobile.me>
2019-03-07 10:53:20 +0700

    create header view holder

---
+++ app/src/main/java/me/suttichai/develop/sticky/HeaderViewHolder.kt
@@ -0,0 +1,6 @@
+package me.suttichai.develop.sticky
+
+import android.view.View
+import androidx.recyclerview.widget.RecyclerView
+
+class HeaderViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView)
