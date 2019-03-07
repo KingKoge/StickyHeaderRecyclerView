f133c1d36235a4883de3f18c1fc3877b40ce1f6d
Suttichai Pawapaocapinun <suttichai@livingmobile.me>
2019-03-07 10:58:36 +0700

    create unknown view holder

---
+++ app/src/main/java/me/suttichai/develop/sticky/UnknownViewHolder.kt
@@ -0,0 +1,6 @@
+package me.suttichai.develop.sticky
+
+import android.view.View
+import androidx.recyclerview.widget.RecyclerView
+
+class UnknownViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView)
