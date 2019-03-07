5fa4e04abb8cb7141d7c87b2524aa359d5a121c2
Suttichai Pawapaocapinun <suttichai@livingmobile.me>
2019-03-07 12:28:26 +0700

    add function bind to body view holder

--- app/src/main/java/me/suttichai/develop/sticky/BodyViewHolder.kt
+++ app/src/main/java/me/suttichai/develop/sticky/BodyViewHolder.kt
@@ -1,6 +1,16 @@
 package me.suttichai.develop.sticky
 
 import android.view.View
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import me.suttichai.develop.sticky.model.Body
 
-class BodyViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView)
+class BodyViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+    var tvBody: TextView = itemView.findViewById(R.id.tvBody)
+
+    fun bind(body: Body?) {
+        if (body != null) {
+            tvBody.text = body.title
+        }
+    }
+}
