bd331cf684e9176c9d47e071fff9fa525cf4a328
Suttichai Pawapaocapinun <suttichai@livingmobile.me>
2019-03-07 12:28:58 +0700

    add function bind to header view holder

--- app/src/main/java/me/suttichai/develop/sticky/HeaderViewHolder.kt
+++ app/src/main/java/me/suttichai/develop/sticky/HeaderViewHolder.kt
@@ -1,6 +1,18 @@
 package me.suttichai.develop.sticky
 
 import android.view.View
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import me.suttichai.develop.sticky.model.Header
 
-class HeaderViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView)
+class HeaderViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+    var tvHeader: TextView = itemView.findViewById(R.id.tvHeader)
+    var tvSection: TextView = itemView.findViewById(R.id.tvSection)
+
+    fun bind(header: Header?) {
+        if (header != null) {
+            tvHeader.text = header.title
+            tvSection.text = header.section
+        }
+    }
+}
