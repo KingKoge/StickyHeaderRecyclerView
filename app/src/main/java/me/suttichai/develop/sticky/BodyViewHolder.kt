d8c9431859fad8ceada00e6ee7e48210634fc8eb
Suttichai Pawapaocapinun <suttichai@livingmobile.me>
2019-03-07 10:53:02 +0700

    create body view holder

---
+++ app/src/main/java/me/suttichai/develop/sticky/BodyViewHolder.kt
@@ -0,0 +1,6 @@
+package me.suttichai.develop.sticky
+
+import android.view.View
+import androidx.recyclerview.widget.RecyclerView
+
+class BodyViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView)
